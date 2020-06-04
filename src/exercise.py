a17e6be35316d27200ac817712e5ff0f0884bec2
cabosh <callum.bosher@gmail.com>
2020-06-04 18:09:43 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
 
+    number = int(input("Enter a number: "))
+
+    if number < 0:
+        print(str(number * -1))
+    else:
+        print(str(number))
+
 if __name__ == '__main__':
     main()
